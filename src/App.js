a9a6a5226d103da491bc926b8978497ff4375e8e
Hawsgard <himanshunayak2003@gmail.com>
2024-01-13 14:44:44 +0530

    auth-pages-created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,42 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/common/Navbar';
-import RuleCreate from './components/RuleCreate';
-import DebugRule from './components/DebugRule';
-import ModifyRule from './components/ModifyRule';
-import TestRule from './components/TestRule';
-import DbFileRule from './components/DbFileRule';
+import { Route, Routes } from "react-router-dom"
+import { useLocation } from "react-router-dom"
+import "./App.css"
+import Navbar from "./components/common/Navbar"
+import RuleCreate from "./components/RuleCreate"
+import DebugRule from "./components/DebugRule"
+import ModifyRule from "./components/ModifyRule"
+import TestRule from "./components/TestRule"
+import DbFileRule from "./components/DbFileRule"
+import Login from "./pages/auth/login.js"
+import Register from "./pages/auth/register.js"
 
 function App() {
+  const location = useLocation()
+
   return (
-    <div className={`bg-[url('./assets/Images/mainbg.jpg')] h-[100vh] w-[100vw] mt-0 mx-0 overflow-hidden`}>
-      <div className='py-[1rem] px-[2rem]'>
-        <Navbar/>
+    <div
+      className={`bg-[url('./assets/Images/mainbg.jpg')] h-[100vh] w-[100vw] mt-0 mx-0 overflow-hidden`}
+    >
+      {location.pathname === "/auth/login" ||
+      location.pathname === "/auth/register" ? (
         <Routes>
-          <Route path='/' element={<RuleCreate/>} />
-          <Route path='/debug' element={<DebugRule/>} />
-          <Route path='/modify' element={<ModifyRule/>} />
-          <Route path='/test' element={<TestRule/>} />
-          <Route path='/dbfilerule' element={<DbFileRule/>} />
+          <Route path="/auth/login" element={<Login />} />
+          <Route path="/auth/register" element={<Register />} />
         </Routes>
-      </div>
+      ) : (
+        <div className="py-[1rem] px-[2rem]">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<RuleCreate />} />
+            <Route path="/debug" element={<DebugRule />} />
+            <Route path="/modify" element={<ModifyRule />} />
+            <Route path="/test" element={<TestRule />} />
+            <Route path="/dbfilerule" element={<DbFileRule />} />
+          </Routes>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
